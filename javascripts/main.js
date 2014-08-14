411f22b0ae22341b0bae0232fdd3605b12290206
oxoxll <oxoxll@qq.com>
2014-08-14 22:20:59 +0800

    copy

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,8 +2,12 @@ function AccountListCtrl($scope) {
     var randomArr = ['a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','G','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
     $scope.accounts = getAccounts();
 
-    $scope.change = function() {
+    $scope.change = function () {
         $scope.accounts = getAccounts();
+    };
+
+    $scope.copy = function (target) {
+        alert('复制成功');
     };
 
     function getAccounts() {
