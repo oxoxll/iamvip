0a18cd55b1aaa4994c639cfda92346951271b6cc
oxoxll <oxoxll@qq.com>
2014-08-17 15:07:33 +0800

    baidu

--- javascripts/main.js
+++ javascripts/main.js
@@ -28,3 +28,5 @@ function AccountListCtrl($scope) {
     }
 }
 
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fac2122fa75b4c3a12135db6aefe4335e' type='text/javascript'%3E%3C/script%3E"));
